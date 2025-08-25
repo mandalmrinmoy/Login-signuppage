63d781139a7b22f08131247d8bb189c50a2d6470
mandalmrinmoy <mrinmoymandal270@gmail.com>
2025-08-25 23:45:31 +0530

    add useState

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,34 @@
+import { useState } from 'react'
 import '../css/login.css'
 
 function Login(){
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        const payload={
+            email:email,
+            password: password
+        }
+        console.log("login",payload)
+    }
     return(
         <>
         <div className="main-container">
             <div className="login-page">
             <h1>LogIn</h1>
-            <form action="" className='form-container'>
-                <input type="email" name="email" placeholder='Email' />
+            <form action="" className='form-container' onSubmit={handleSubmit}>
+                <input type="email" name="email" placeholder='Email'  value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
                 <div className="password">
-                <input type="password" name="password" placeholder="Password" />
-                <i class="fa-solid fa-eye-slash"></i>
+                <input type="password" name="password" placeholder="Password"  value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+                <i className="fa-solid fa-eye-slash"></i>
                 </div>
                 <p className='forgot-p'>Forgot password?</p>
                 <button type='submit' className='btn login-btn'>LogIn</button>
                 <p className='account'>Don't have an account? <span>Signup</span></p>
                 <div className='or'><div className="line-container"></div><p>Or</p><div className="line-container"></div></div>
-                <button className='btn facebook-btn'><i class="fa-brands fa-facebook"></i><p>LogIn with Facebook</p></button>
-                <button className='btn google-btn'><i class="fa-brands fa-google"></i><p>Login with Google</p></button>
+                <button className='btn facebook-btn'><i className="fa-brands fa-facebook"></i><p>LogIn with Facebook</p></button>
+                <button className='btn google-btn'><i className="fa-brands fa-google"></i><p>Login with Google</p></button>
             </form>
         </div>
         </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,23 +1,46 @@
+import { useState } from "react"
 import "../css/signup.css"
 
 function Signup(){
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const [confirmPassword,setConfirmPassword]=useState("")
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        const payload={
+            email:email,
+            password:password,
+            confirmPassword:confirmPassword
+        }
+        if(password===confirmPassword){
+            alert("Your account create successfully")
+            console.log("output: ",payload)
+        }else{
+            alert("password and confirm password are not matched")
+        }
+        setEmail("");
+        setPassword("");
+        setConfirmPassword("")
+        
+    }
+    
     return(
         <>
         <div className="main-container">
             <div className="signup-page">
             <h1>Signup</h1>
-            <form action="" className="form-container">
-                <input type="email" name="email" placeholder="Email"/>
-                <input type="password" name="password" placeholder="Create password"/>
+            <form action="" className="form-container" onSubmit={handleSubmit}>
+                <input type="email" name="email" placeholder="Email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
+                <input type="password" name="password" placeholder="Create password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
                 <div className="password">
-                <input type="password" name="confirmPassword" placeholder="Confirm password" />
-                <i class="fa-solid fa-eye-slash"></i>
+                <input type="password" name="confirmPassword" placeholder="Confirm password" value={confirmPassword} onChange={(e)=>{setConfirmPassword(e.target.value)}}/>
+                <i className="fa-solid fa-eye-slash"></i>
                 </div>
-                <button className='btn signup-btn'>Signup</button>
+                <button className='btn signup-btn' type="submit">Signup</button>
                 <p className='account'>Already have an account?<span>Login</span></p>
                 <div className='or'><div className="line-container"></div><p>Or</p><div className="line-container"></div></div>
-                <button className='btn facebook-btn'><i class="fa-brands fa-facebook"></i><p>LogIn with Facebook</p></button>
-                <button className='btn google-btn'><i class="fa-brands fa-google"></i><p>Login with Google</p></button>
+                <button className='btn facebook-btn'><i className="fa-brands fa-facebook"></i><p>LogIn with Facebook</p></button>
+                <button className='btn google-btn'><i className="fa-brands fa-google"></i><p>Login with Google</p></button>
             </form>
         </div>
         </div>
